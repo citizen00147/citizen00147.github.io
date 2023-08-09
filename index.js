4ee009224a391e158568f5c7b9f4af537e862d16
mike <85201967+citizen00147@users.noreply.github.com>
2023-08-08 20:22:53 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const aboutButton = document.getElementById("aboutMeDetails");
+const aboutProject = document.getElementById("aboutMe");
+
+aboutButton.addEventListener("click", function () {
+  aboutProject.showModal();
+});
